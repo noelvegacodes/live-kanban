173cc2650de976a9269b8a5385952ff373258eb7
noelvegacodes <noelvegacodes@gmail.com>
2023-08-13 15:34:49 -0400

    init-partykit

---
+++ src/routes/room/[room]/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { enhance } from "$app/forms";
+    import {page} from "$app/stores"
+    import PartySocket from 'partysocket';
+
+    let msg = ""
+    let messages: string[] = []
+
+    const socket = new PartySocket({
+        host: 'codefork-party.noelvegacodes.partykit.dev',
+        room: $page.params.room
+    })
+    console.log($page.params.room)
+
+    socket.addEventListener("message", (evt) => {
+        const data = JSON.parse(evt.data);
+        if(data.type === "chatmsg") {
+            messages = [...messages, data.msg]
+        }
+        console.log(evt.data)
+    })
+
+
+</script>
+
+<ul>
+    {#each messages as message}
+        <li>{message}</li>
+    {/each}
+</ul>
+
+<form on:submit|preventDefault={() => {
+    console.log("submit")
+    socket.send(JSON.stringify({type: "chatmsg", msg}))
+}}>
+    <input type="text" bind:value={msg} />
+</form>
