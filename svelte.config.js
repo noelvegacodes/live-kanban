be58eff420a9d30b1606752ec0def82102d9d18d
noelvegacodes <noelvegacodes@gmail.com>
2023-08-13 15:57:41 -0400

    test

--- svelte.config.js
+++ svelte.config.js
@@ -11,14 +11,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
-		vite: {
-			// other options ....
-			optimizeDeps: {
-				exclude: ['lib/partykit.ts']
-			}
-			// in case you want to interact with rollup you can use
-		}
+		adapter: adapter()
 	}
 };
 
